525ec583dbbdf20045b92d2f8ee554ae6f29db4d
Leon Schmidtchen <leon.schmidtchen@gmail.com>
2021-04-26 21:32:59 +0200

    Create first API class
    First part of implementing the viewer API documentation we received.
    
    This is only the class, it is not used in the main.js yet.
    
    Accidentally left out this file in the previous commit

---
+++ src/js/viewer/ViewerViewState.js
@@ -0,0 +1,11 @@
+"use strict";
+
+export class ViewerViewState{
+    
+    fov = 90;   // : Number // Field of view (in degrees)
+
+    latov = 0; // : Number // View latitude (in rad; 0 is horizontal)
+
+    lonov = 0; // : Number // View longitude (in rad; 0 looks to east)
+
+}
