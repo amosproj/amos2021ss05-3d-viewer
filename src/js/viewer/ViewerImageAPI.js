113c4e55799fff38332c2b7b588706fca15e3ee4
Mm24 <maria.monzon@fau.de>
2021-05-02 14:04:45 +0200

    Refactor skeleton ViewerImageAPI

--- src/js/viewer/ViewerImageAPI.js
+++ src/js/viewer/ViewerImageAPI.js
@@ -1,14 +1,15 @@
 "use strict";
+import { ViewerImage } from "ViewerImageAPI.js";
 
-// Panorama Image
+// Specific API for Panorama Image(s)
 export class ViewerImageAPI {
 
     constructor() {
         // hardcoded to work with assets/ for now
 
         this.metadata;
-
         const jsonImageDataFilepath = "../assets/data.json";
+        this.viewer_image = new ViewerImage ()
 
         $.getJSON(jsonImageDataFilepath, function(data) {
            this.metadata = data;
@@ -16,5 +17,19 @@ export class ViewerImageAPI {
         });
 
     }
+    
+    all ( callback ) {
+     //    Get all panorama images.
+    // Parameters:  Function called with all images ([ViewerImage]): Array of panorama images
+    }
 
+    changed (  ) {
+        //  Signal changed image data (e.g. hidden flag) to the viewer.
+    }
+
+    get get() {
+        //      Get the currently displayed panorama image.
+
+        return this.viewer_image;
+    }
 }
