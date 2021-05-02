d8a7ef420bf6f1dcb8b5f8420e6649e80af48cc9
Mm24 <maria.monzon@fau.de>
2021-05-02 17:23:19 +0200

    update ViewerImageAPI comments

--- src/js/viewer/ViewerImageAPI.js
+++ src/js/viewer/ViewerImageAPI.js
@@ -1,19 +1,25 @@
 "use strict";
-import { ViewerImage } from "ViewerImageAPI.js";
+//import { ViewerImage } from "ViewerImage.js";
 
 // Specific API for Panorama Image(s)
 export class ViewerImageAPI {
 
     constructor() {
         // hardcoded to work with assets/ for now
-
-        this.metadata;
         const jsonImageDataFilepath = "../assets/data.json";
-        this.viewer_image = new ViewerImage ()
+        this.viewer_image; //= new ViewerImage()
+        this.metadata; 
+        // The file «data.json» contains the metadata defining the panorama image locations.
+            //"images" Array Images Array
+            //"lon0" Number Reference longitude of model (WGS 84)
+            //"lat0" Number Reference latitude of model (WGS 84)
+            //"floors" Object Floors Object
 
+            
         $.getJSON(jsonImageDataFilepath, function(data) {
            this.metadata = data;
            console.log(data); 
+
         });
 
     }
