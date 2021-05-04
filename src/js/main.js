ad739513e98b8a9b43944dfdcafcd8d1ac9009c2
Mm24 <maria.monzon@fau.de>
2021-05-04 16:09:07 +0200

    main.js has integration to show the map as sprite dinammicaly changing

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,14 @@
 "use strict";
+//import * as THREE from './libs/three.module.js'
 import { ViewerImageAPI } from "./viewer/ViewerImageAPI.js";
 import { ViewerViewState } from "./viewer/ViewerViewState.js";
+
 // import { GUI } from './jsm/libs/dat.gui.module.js';
 
 let camera, scene, renderer;
-//var cameraOrtho, sceneOrtho, map_renderer;
+let  cameraOrtho, sceneOrtho;
 var geometry, material, mesh;
-
-let cameraOrtho, sceneOrtho;
-let spriteBR;
+let spriteMap;
 
 let onPointerDownMouseX = 0, onPointerDownMouseY = 0,
     longitude = 0, onPointerDownLon = 0,
@@ -29,6 +29,15 @@ function init() {
     camera = new THREE.PerspectiveCamera(DEFAULT_FOV, window.innerWidth / window.innerHeight, 1, 1100);
     scene = new THREE.Scene();
 
+     // Create the Overlay scene
+     
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    cameraOrtho = new THREE.OrthographicCamera( - width / 2, width / 2, height / 2, - height / 2, 1, 10 );
+    cameraOrtho.position.z = 10;
+    sceneOrtho = new THREE.Scene();
+
     // Create a Sphere for the image texture to be displayed on
     const sphere = new THREE.SphereGeometry(500, 60, 40);
     // invert the geometry on the x-axis so that we look out from the middle of the sphere
@@ -43,41 +52,20 @@ function init() {
     const mesh = new THREE.Mesh(sphere, material);
     scene.add(mesh);
 
-    
+
     //Create new camera for 2D display
-    
-    const width = window.innerWidth;
-    const height = window.innerHeight;
 
-    cameraOrtho = new THREE.OrthographicCamera( - width / 2, width / 2, height / 2, - height / 2, 1, 10 );
-    cameraOrtho.position.z = 10;
-    sceneOrtho = new THREE.Scene();
-
-
-    //ADD MAP TEXTURE 
-	//var mapTexture = THREE.ImageUtils.loadTexture( '../assets/map0.png' ); // var map = new THREE.TextureLoader().load( "../assets/map0.png" );
-    // var mat = new THREE.SpriteMaterial( { map: map, color: 0xffffff } );
-	//var mapMaterial = new THREE.SpriteMaterial( { map: mapTexture, useScreenCoordinates: true} ); //, alignment: THREE.SpriteAlignment.topLeft  } );
-	//var sprite = new THREE.Sprite( mapMaterial );
-	//sprite.position.set( 50, 50, 0 );
-	//sprite.scale.set( 64, 64, 1.0 ); // imageWidth, imageHeight
-	//scene.add( sprite );
-
-    // create sprites
-    const mapTexture = new THREE.TextureLoader().load( "../assets/map0.png", createHUDSprites );
-    const materialMap = new THREE.SpriteMaterial( { map: mapTexture, color: 0xffffff, fog: true } );
+    const textureLoader = new THREE.TextureLoader();
+    textureLoader.load( "../assets/map-small.jpg", createHUDSprites );
 
 
     // create the renderer, and embed the attributed dom element in the html page
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
-    //Render 2D Map
-    renderer.clearDepth();
-    renderer.render( sceneOrtho, cameraOrtho );
-    container.appendChild(renderer.domElement);
-
-
+	//renderer.autoClear = false; // To allow render overlay on top of panorama scene
+	
+    document.body.appendChild( renderer.domElement );
 
     // link event listeners to the corresponding functions
     container.addEventListener('pointerdown', onPointerDown);
@@ -101,6 +89,7 @@ function animate() {
 
     requestAnimationFrame(animate);
     update();
+    //render(); 
 
 }
 
@@ -112,11 +101,9 @@ function update() {
     const x = 500 * Math.sin(phi) * Math.cos(theta);
     const y = 500 * Math.cos(phi);
     const z = 500 * Math.sin(phi) * Math.sin(theta);
-
     camera.lookAt(x, y, z);
-
-    renderer.render(scene, camera);
-
+    //renderer.clear();
+    renderer.render( scene, camera );
 }
 
 // this event listener is called when the user *begins* moving the picture
@@ -164,30 +151,34 @@ function onDocumentMouseWheel(event) {
 
 }
 
+
 function onWindowResize() {
 
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    
+    cameraOrtho.left = - width / 2;
+    cameraOrtho.right = width / 2;
+    cameraOrtho.top = height / 2;
+    cameraOrtho.bottom = - height / 2;
+    cameraOrtho.updateProjectionMatrix();
+    updateHUDSprites();
+
+   // renderer.setSize(window.innerWidth, window.innerHeight);
 
 }
 
 
-
 function createHUDSprites( texture ) {
-
+    //Texture is Map
     const material = new THREE.SpriteMaterial( { map: texture } );
-
     const width = material.map.image.width;
     const height = material.map.image.height;
-
-
-    spriteBR = new THREE.Sprite( material );
-    spriteBR.center.set( 1.0, 0.0 );
-    spriteBR.scale.set( width, height, 1 );
-    sceneOrtho.add( spriteBR );
-
+    spriteMap = new THREE.Sprite( material );
+    spriteMap.center.set( 0.0, 1.0 );
+    spriteMap.scale.set(  width, - height, 1 ); // bottom right
+    sceneOrtho.add( spriteMap );
     updateHUDSprites();
 
 }
@@ -196,7 +187,17 @@ function updateHUDSprites() {
 
     const width = window.innerWidth / 2;
     const height = window.innerHeight / 2;
+    spriteMap.position.set(- width,- height, 1 );// bottom right
 
-    spriteBR.position.set( width, - height, 1 ); // bottom right
-    
 }
+
+function render() {
+
+    const time = Date.now() / 1000;
+    //renderer.clear();
+	//renderer.render( scene, camera );
+	renderer.clearDepth();
+    renderer.render( sceneOrtho, cameraOrtho );
+
+}
+
