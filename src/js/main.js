103a67c99ae16e051c7f4f520a85204a22c3083e
Mm24 <maria.monzon@fau.de>
2021-05-04 11:55:52 +0200

    update style css to display the static map

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,11 @@ import { ViewerViewState } from "./viewer/ViewerViewState.js";
 // import { GUI } from './jsm/libs/dat.gui.module.js';
 
 let camera, scene, renderer;
-var cameraOrtho, sceneOrtho, map_renderer;
+//var cameraOrtho, sceneOrtho, map_renderer;
 var geometry, material, mesh;
 
+let cameraOrtho, sceneOrtho;
+let spriteBR;
 
 let onPointerDownMouseX = 0, onPointerDownMouseY = 0,
     longitude = 0, onPointerDownLon = 0,
@@ -27,7 +29,6 @@ function init() {
     camera = new THREE.PerspectiveCamera(DEFAULT_FOV, window.innerWidth / window.innerHeight, 1, 1100);
     scene = new THREE.Scene();
 
-
     // Create a Sphere for the image texture to be displayed on
     const sphere = new THREE.SphereGeometry(500, 60, 40);
     // invert the geometry on the x-axis so that we look out from the middle of the sphere
@@ -42,11 +43,38 @@ function init() {
     const mesh = new THREE.Mesh(sphere, material);
     scene.add(mesh);
 
+    
+    //Create new camera for 2D display
+    
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    cameraOrtho = new THREE.OrthographicCamera( - width / 2, width / 2, height / 2, - height / 2, 1, 10 );
+    cameraOrtho.position.z = 10;
+    sceneOrtho = new THREE.Scene();
+
+
+    //ADD MAP TEXTURE 
+	//var mapTexture = THREE.ImageUtils.loadTexture( '../assets/map0.png' ); // var map = new THREE.TextureLoader().load( "../assets/map0.png" );
+    // var mat = new THREE.SpriteMaterial( { map: map, color: 0xffffff } );
+	//var mapMaterial = new THREE.SpriteMaterial( { map: mapTexture, useScreenCoordinates: true} ); //, alignment: THREE.SpriteAlignment.topLeft  } );
+	//var sprite = new THREE.Sprite( mapMaterial );
+	//sprite.position.set( 50, 50, 0 );
+	//sprite.scale.set( 64, 64, 1.0 ); // imageWidth, imageHeight
+	//scene.add( sprite );
+
+    // create sprites
+    const mapTexture = new THREE.TextureLoader().load( "../assets/map0.png", createHUDSprites );
+    const materialMap = new THREE.SpriteMaterial( { map: mapTexture, color: 0xffffff, fog: true } );
+
 
     // create the renderer, and embed the attributed dom element in the html page
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
+    //Render 2D Map
+    renderer.clearDepth();
+    renderer.render( sceneOrtho, cameraOrtho );
     container.appendChild(renderer.domElement);
 
 
@@ -143,4 +171,32 @@ function onWindowResize() {
 
     renderer.setSize(window.innerWidth, window.innerHeight);
 
+}
+
+
+
+function createHUDSprites( texture ) {
+
+    const material = new THREE.SpriteMaterial( { map: texture } );
+
+    const width = material.map.image.width;
+    const height = material.map.image.height;
+
+
+    spriteBR = new THREE.Sprite( material );
+    spriteBR.center.set( 1.0, 0.0 );
+    spriteBR.scale.set( width, height, 1 );
+    sceneOrtho.add( spriteBR );
+
+    updateHUDSprites();
+
+}
+
+function updateHUDSprites() {
+
+    const width = window.innerWidth / 2;
+    const height = window.innerHeight / 2;
+
+    spriteBR.position.set( width, - height, 1 ); // bottom right
+    
 }
