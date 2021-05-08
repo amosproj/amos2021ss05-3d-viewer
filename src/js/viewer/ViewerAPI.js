1ef3a53b51f3932a3143b62db47739b7e1e0935e
Nes55 <73950780+Nes55@users.noreply.github.com>
2021-05-08 16:16:22 +0200

    Update ViewerAPI.js
    API doc

--- src/js/viewer/ViewerAPI.js
+++ src/js/viewer/ViewerAPI.js
@@ -5,45 +5,48 @@ export class ViewerAPI {
     constructor() {
         //list of all images
         this.images = [];
-        let temp = new Array();
-        
-        // access JSON file
-        const jsonImageDataFilepath = "../assets/data.json";
+        $.jsonImageDataFilepath,
+            function(distance) {
 
-        // save only index 0 to 2 of each images as THREE.Vector3
-        $.getJSON(jsonImageDataFilepath, function(data) {
-            $.each( data.images, function( key, val ) {
-                //console.log(key+":"+val);
-                temp.push(val);
-            });
-        });
-        //alert(JSON.stringify(temp));
-        console.log(temp);
-        // $.each(temp,function(key,val){
-        //     console.log(key+":"+val);
-        // });
+                let temp = new Array();
 
-    }
+                // access JSON file
+                const jsonImageDataFilepath = "../assets/data.json";
 
-    //Move the view to the given position.
-    move ( lon,  lat,  z ){
+                // save only index 0 to 2 of each images as THREE.Vector3
+                $.getJSON(jsonImageDataFilepath, function(data) {
+                    $.each(data.images, function(key, val) {
+                        //console.log(key+":"+val);
+                        temp.push(val);
+                    });
+                });
+                //alert(JSON.stringify(temp));
+                console.log(temp);
+                // $.each(temp,function(key,val){
+                //     console.log(key+":"+val);
+                // });
 
-        // current lon, lat, z
-        const vec1 = new THREE.Vector3( lon, lat, z );
+            }
 
-        // compare between vectors
-        const distance = [];
+        //Move the view to the given position.
+        move(lon, lat, z) {
 
-        for (i = 0; i < this.images.length; i++){
-            distance.push = vec1.distanceTo(this.images[i]);
+            // current lon, lat, z
+            const vec1 = new THREE.Vector3(lon, lat, z);
+
+            // compare between vectors
+            const distance = [];
+
+            for (i = 0; i < this.images.length; i++) {
+                distance.push = vec1.distanceTo(this.images[i]);
+            }
+
+            // get the index of the image which has the shotest distance
+            const index = distance.indexOf(Math.min.apply(Math, distance));
+
+            // move to the index of this image //TODO
+
         }
-        
-        // get the index of the image which has the shotest distance
-        const index = distance.indexOf(Math.min.apply(Math, distance));
 
-        // move to the index of this image //TODO
 
     }
-
-
-}
