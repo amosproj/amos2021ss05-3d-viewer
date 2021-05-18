4f6def05bf4ab46d5351ff861992793c961961b3
Lance <53155359+Lancelof2019@users.noreply.github.com>
2021-05-18 19:21:36 +0200

    Create ViewerState.js
    Create ViewerState class and parameters

---
+++ src/js/viewer/ViewerState.js
@@ -0,0 +1,22 @@
+export class ViewerState {
+
+    constructor(panoLon, panoLat, imageIdx, name,[fov, latov,lonov] ) {
+
+        //this.big =
+    
+        this.floor = name; // : String // Name of floor which contains this image
+
+        this.imageNum = imageIdx; // : Number // Image number
+    
+        this.loc = [panoLon, panoLat]; // : [Number] // WGS 84 coordinates [longitude, latitude, z] of this image
+
+        //this.fov = fov; // : Number // Field of view (in degrees)
+       // viewerViewState1= new ViewerViewState(DEFAULT_FOV, 0, 0)
+       this.view=[fov, latov, lonov];
+      
+
+
+
+    }
+
+}
