b59804d32328a399a1dedd7829a6ee5cdbe5286a
Lance <53155359+Lancelof2019@users.noreply.github.com>
2021-05-24 19:17:33 +0200

    Update main.js
     basicSetUp has been updated

--- src/js/main.js
+++ src/js/main.js
@@ -253,12 +253,47 @@ function render() {
    
 function basicSetUp(){
 
+  
+    var loc_para1;
+    var loc_para2;
+    var loc_para3;
+   var imageId;
+   var floors_name;
+   
+   if(trackPosVert==0&&trackPosLat==null||trackPosVert==0&&trackPosLon==null){
 
-    var loc_para1=trackPosLon;
-    var loc_para2=trackPosLat;
-    var loc_para3=trackPosVert;
-    var imageId=trackImageID;
-    var floors_name=trackFloorName;
+     loc_para1=viewerFloorAPI.floors[1].viewerImages[0].pos[0];
+     loc_para2=viewerFloorAPI.floors[1].viewerImages[0].pos[1];
+    loc_para3=viewerFloorAPI.floors[1].viewerImages[0].pos[2];
+    imageId=viewerFloorAPI.floors[1].viewerImages[0].id;
+    floors_name=viewerFloorAPI.floors[1].name;
+      
+
+
+
+ }
+  else{
+
+
+    loc_para1=trackPosLon;
+    loc_para2=trackPosLat;
+   loc_para3=trackPosVert;
+   imageId=trackImageID;
+   floors_name=trackFloorName;
+
+
+  }
+  
+
+
+
+  console.log("The para of loc_para1 ")
+  console.log(loc_para1);
+
+   
+    console.log("The para of loc_para1 ")
+    console.log(loc_para1);
+  
     var latov_rad=viewerViewState.latov* Math.PI / 180.0;
     var lonov_rad=viewerViewState.lonov* Math.PI / 180.0;
     var viewer_fov=viewerViewState.fov;
@@ -269,7 +304,6 @@ function basicSetUp(){
    viewerState=new ViewerState([loc_para1,loc_para2,loc_para3],imageId,floors_name,[viewer_fov,latov_rad,lonov_rad]) ;
    view_para=viewerState.view;
    viewerAPI.viewerVersionAPI=new ViewerVersionAPI(vMajor, vMinor,view_para);
-
 
 }
 
