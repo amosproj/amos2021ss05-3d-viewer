bf6c431c5bfbc1ed92764f312c04d0e386258717
Lance <53155359+Lancelof2019@users.noreply.github.com>
2021-05-25 18:19:18 +0200

    Update main.js
    changed the format

--- src/js/main.js
+++ src/js/main.js
@@ -211,11 +211,11 @@ function onDoubleClick(event) {
 
     viewerAPI.move(newPos[0], newPos[1], currentPos[2]);
 
-    trackPosLon=currentPos[0];
-    trackPosLat=currentPos[1];
-    trackPosVert=currentPos[2];
-    trackImageID=viewerImageAPI.currentImageId;
-    trackFloorName=viewerImageAPI.currentImage.floor;
+    trackPosLon = currentPos[0];
+    trackPosLat = currentPos[1];
+    trackPosVert = currentPos[2];
+    trackImageID = viewerImageAPI.currentImageId;
+    trackFloorName = viewerImageAPI.currentImage.floor;
 }
 
 function render() {
@@ -230,35 +230,37 @@ function render() {
 
 
    
-function basicSetUp(){
+function basicSetUp() {
    var loc_para1;
    var loc_para2;
    var loc_para3;
    var imageId;
    var floors_name;
-   if(trackPosVert==0&&trackPosLat==null||trackPosVert==0&&trackPosLon==null){
-      imageId=viewerFloorAPI.floors[viewerFloorAPI.currentFloorId].viewerImages[viewerFloorAPI.currentFloorId].id;
-      floors_name= viewerFloorAPI.floors[viewerFloorAPI.currentFloorId].name;
-      loc_para1=viewerFloorAPI.floors[viewerFloorAPI.currentFloorId].viewerImages[viewerFloorAPI.currentFloorId].pos[0];
-      loc_para2=viewerFloorAPI.floors[viewerFloorAPI.currentFloorId].viewerImages[viewerFloorAPI.currentFloorId].pos[1];
-      loc_para3=viewerFloorAPI.floors[viewerFloorAPI.currentFloorId].viewerImages[viewerFloorAPI.currentFloorId].pos[2];
-  }
-   else{
-     loc_para1=trackPosLon;
-     loc_para2=trackPosLat;
-     loc_para3=trackPosVert;
-     imageId=trackImageID;
-     floors_name=trackFloorName;
-  }
-   var latov_rad=viewerViewState.latov* Math.PI / 180.0;
-   var lonov_rad=viewerViewState.lonov* Math.PI / 180.0;
-   var viewer_fov=viewerViewState.fov;
-   var vMajor=viewerAPI.MAJOR;
-   var vMinor=viewerAPI.MINOR;
-   var view_para=[];
-   viewerState=new ViewerState([loc_para1,loc_para2,loc_para3],imageId,floors_name,[viewer_fov,latov_rad,lonov_rad]) ;
-   view_para=viewerState.view;
-   viewerAPI.viewerVersionAPI=new ViewerVersionAPI(vMajor, vMinor,view_para);
+   if (trackPosVert == 0 && trackPosLat == null || trackPosVert == 0 && trackPosLon == null) {
+      imageId = viewerFloorAPI.floors[viewerFloorAPI.currentFloorId].viewerImages[viewerFloorAPI.currentFloorId].id;
+      floors_name = viewerFloorAPI.floors[viewerFloorAPI.currentFloorId].name;
+      loc_para1 = viewerFloorAPI.floors[viewerFloorAPI.currentFloorId].viewerImages[viewerFloorAPI.currentFloorId].pos[0];
+      loc_para2 = viewerFloorAPI.floors[viewerFloorAPI.currentFloorId].viewerImages[viewerFloorAPI.currentFloorId].pos[1];
+      loc_para3 = viewerFloorAPI.floors[viewerFloorAPI.currentFloorId].viewerImages[viewerFloorAPI.currentFloorId].pos[2];
+ 
+   } else {
+     loc_para1 = trackPosLon;
+     loc_para2 = trackPosLat;
+     loc_para3 = trackPosVert;
+     imageId = trackImageID;
+     floors_name = trackFloorName;
+  
+   }
+   var latov_rad = viewerViewState.latov * Math.PI / 180.0;
+   var lonov_rad = viewerViewState.lonov * Math.PI / 180.0;
+   var viewer_fov = viewerViewState.fov;
+   var vMajor = viewerAPI.MAJOR;
+   var vMinor = viewerAPI.MINOR;
+   var view_para = [];
+   viewerState = new ViewerState( [loc_para1,loc_para2,loc_para3],imageId,floors_name,[viewer_fov,latov_rad,lonov_rad] ) ;
+   view_para = viewerState.view;
+   viewerAPI.viewerVersionAPI = new ViewerVersionAPI(vMajor, vMinor,view_para);
+ 
 }
 
 
