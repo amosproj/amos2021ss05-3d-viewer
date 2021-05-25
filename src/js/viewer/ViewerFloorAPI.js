39273f9148b05c46010c6b1246a6425bb763b101
leonopulos <Leon.Schmidtchen@gmail.com>
2021-05-25 18:32:41 +0200

    Remove Console logging from PR #57

--- src/js/viewer/ViewerFloorAPI.js
+++ src/js/viewer/ViewerFloorAPI.js
@@ -1,12 +1,10 @@
 "use strict";
 
 import { distanceWGS84TwoPoints } from "./Globals.js";
-//let currentImage;
-let currentImagepara;
+
 export class ViewerFloorAPI {
    
     constructor(data, viewerImageAPI) {
-       // let currentImage;
         // The file «data.json» contains the metadata defining the panorama image locations.
             //"images" Array Images Array
             //"lon0" Number Reference longitude of model (WGS 84)
@@ -29,8 +27,7 @@ export class ViewerFloorAPI {
                 currentFloor.i.forEach((interval) => {
                     if (currentImage.id >= interval[0] && currentImage.id <= interval[1]) {
                         currentImage.floor = key;
-                       console.log("Currecut image id :"+currentImage.id);
-                        //id_temp=currentImage.id;
+                        
                         // dx, dy distance in kilometers
                         const [dx, dy] = distanceWGS84TwoPoints(data.lon0, data.lat0, currentImage.pos[0], currentImage.pos[1]);
 
@@ -40,18 +37,11 @@ export class ViewerFloorAPI {
                         currentImage.mapOffset = [offsetX, offsetY];
 
                         currentFloor.viewerImages.push(currentImage);
-                       
-                        
                     }
-                    
                  });
-
-                 //currentImagepara=currentImage;
             });
 
-            this.floors.push(currentFloor);
-           // console.log(currentFloor);
-            
+            this.floors.push(currentFloor);            
         });
 
         //lowest floor will be at lowest index and highest floor at floors.length-1
@@ -59,8 +49,6 @@ export class ViewerFloorAPI {
 
         this.currentFloorId = 0.0;
         viewerImageAPI.currentImageId = this.floors[this.currentFloorId].i[0][0];
-   
-       
     }
 
     all(callback) {
