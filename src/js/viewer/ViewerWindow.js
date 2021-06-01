95c4778d23d0804a6afcfe16fef4f5a274d02b89
leonopulos <Leon.Schmidtchen@gmail.com>
2021-06-01 19:52:58 +0200

    Added external starter html as described by Thomas
    Project now needs to be started from starter.html
    index.html reads the value of "onLoad" in the url, then finds the correct function (window.parent.callback as added before in starter), and finally calls it with appropriate parameters. From the window.parent.callback finally the viewerAPI is created via the call to viewerAsync.
    The main.js file is now no longer referenced anywhere

--- src/js/viewer/ViewerWindow.js
+++ src/js/viewer/ViewerWindow.js
@@ -4,11 +4,8 @@ import { ViewerAPI } from "./ViewerAPI.js";
 
 export class ViewerWindow{
 
-    constructor() {
-        this.window = window;
-    }
     
-    viewerAsync(baseUrl, callback) {
+    static viewerAsync(baseUrl, callback) {
         const api = new ViewerAPI(baseUrl);
         callback(api);
     }
