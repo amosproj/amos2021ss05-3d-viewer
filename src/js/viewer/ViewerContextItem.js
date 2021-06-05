60f4ad4fe3ce81284a43a4653f874bd4627f14ed
Lance <53155359+Lancelof2019@users.noreply.github.com>
2021-06-05 17:49:30 +0200

    ViewerContextItem class
    create new file ViewerContextItem

---
+++ src/js/viewer/ViewerContextItem.js
@@ -0,0 +1,30 @@
+class ViewerContextItem{
+ 
+    constructor(){
+            
+    $(function(){ 
+        $.contextMenu({
+             selector: 'html', //.context-menu-one
+             callback: function(key, options) {
+             var msg = "clicked: " + key;
+             window.console && console.log(msg) || alert(msg); 
+            },//
+             items: {
+             "edit": {name: "Edit", icon: "edit"},
+             "cut": {name: "Cut", icon: "cut"},
+             "copy": {name: "Copy", icon: "copy"},
+             "paste": {name: "Paste", icon: "paste"},
+             "delete": {name: "Delete", icon: "delete"},
+             "sep1": "-------------------",
+             "quit": {name: "Quit", icon: "quit"}
+           }
+        });
+
+      $('html').on('click', function(e){
+      console.log('clicked', this);
+       })
+    });
+
+}
+
+}
