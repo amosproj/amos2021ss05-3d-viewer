72b9e4efa556604cafb9e2efbafde353161cfcff
Tong Yao <51960333+ChillYao@users.noreply.github.com>
2021-06-06 13:51:35 +0200

    did some small refinement.

--- src/js/viewer/EventLayer.js
+++ src/js/viewer/EventLayer.js
@@ -23,21 +23,19 @@ export class EventLayer {
         //Parameters: 
         //xy EventPosition:  Pointer position
         //location THREE.Vector3 : Local coordinates for pointer position+
-        var items = {
-            "edit": { name: "Edit", icon: "edit" },
-            "cut": { name: "Cut", icon: "cut" },
-        };
 
+        //Creating callback function for context menu item.
         let callback = function (key, options) {
             var msg = 'clicked: ' + key;
             (window.console && console.log(msg)) || alert(msg)
         };
 
-        let itemEdit = new ViewerContextItem(callback, items.edit.icon, null, items.edit.name);
+        //Creating item objects
+        let itemEdit = new ViewerContextItem(callback, "edit", null, "Edit");
+        let itemCut = new ViewerContextItem(callback, "cut", null, "Cut");
 
-        let itemCut = new ViewerContextItem(callback, items.cut.icon, null, items.cut.name);
-
-        var listOfItems = { "edit": itemEdit, "cut": itemCut };
+        //Creating list of item objects.
+        let listOfItems = { "edit": itemEdit, "cut": itemCut };
 
         return listOfItems;
     }
