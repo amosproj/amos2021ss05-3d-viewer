f6eaba17a675710b18e23766ff2b68b9b757eeb6
Tong Yao <51960333+ChillYao@users.noreply.github.com>
2021-06-07 17:54:34 +0200

    Get pointer location when right mouse is clicked.

--- src/js/viewer/EventLayer.js
+++ src/js/viewer/EventLayer.js
@@ -1,25 +1,13 @@
 "use strict";
 import { ViewerContextItem } from "./ViewerContextItem.js";
 
-// Event position in viewer
-
-export class EventPosition {
-    constructor() {
-
-        this.x; //  Number // y coordinate of this image on the floor
-
-        this.y; // Number // y coordinate of this image on the floor
-    }
-
-}
-
-
 export class EventLayer {
     constructor() {
         this.viewer_contex;
     }
 
     vwr_oncontext(xy, location) {
+        console.log("xy: ", xy);
         //Parameters: 
         //xy EventPosition:  Pointer position
         //location THREE.Vector3 : Local coordinates for pointer position+
@@ -27,7 +15,7 @@ export class EventLayer {
         //Creating callback function for context menu item.
         let callback = function (key, options) {
             var msg = 'clicked: ' + key;
-            (window.console && console.log(msg)) || alert(msg)
+            (window.console && console.log(msg)) || alert(msg);
         };
 
         //Creating item objects
---
+++ src/js/viewer/EventPosition.js
@@ -0,0 +1,10 @@
+"use strict";
+
+export class EventPosition {
+    constructor(event) {
+
+        this.x = event.clientX;
+        this.y = event.clientY;
+
+    }
+}
