fb35768b05486f782286f7b36b0499fee667f6b0
Lance <53155359+Lancelof2019@users.noreply.github.com>
2021-06-29 21:04:23 +0200

    Update ViewerPanoAPI.js

--- src/js/viewer/ViewerPanoAPI.js
+++ src/js/viewer/ViewerPanoAPI.js
@@ -42,7 +42,24 @@ export class ViewerPanoAPI {
             this.meshCheckRightClick(event);
         });
         panoViewer.addEventListener('pointermove', (event) => this.meshCheckMouseOver(event));
-
+       /////new changes
+	    panoViewer.addEventListener('keydown', (event) => {
+            const keyName = event.key;
+          
+            if (keyName === 'Control') {
+              // do not alert when only Control key is pressed.
+              return;
+            }
+          
+            if (event.ctrlKey) {
+              // Even though event.key is not 'Control' (e.g., 'a' is pressed),
+              // event.ctrlKey may be true if Ctrl key is pressed at the same time.
+              alert(`Combination of ctrlKey + ${keyName}`);
+            } else {
+              alert(`Key pressed ${keyName}`);
+            }
+          }, false);
+	 ////
 
         this.display(this.viewerAPI.image.currentImageId);
     }
